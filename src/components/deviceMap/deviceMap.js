2be1f906e3233ec6900423771889a58705c8d1f9
REDMOND\v-swsan <v-swsan@microsoft.com>
2017-12-05 21:39:19 -0800

    final change

--- src/components/deviceMap/deviceMap.js
+++ src/components/deviceMap/deviceMap.js
@@ -11,6 +11,7 @@ import StaticMapWithOutDevices from '../../assets/icons/staticMapWithOutDevices.
 import lang from '../../common/lang';
 import config from '../../common/config';
 import DashboardPanel from '../dashboardPanel/dashboardPanel';
+
 import './deviceMap.css';
 
 class DeviceMap extends Component {
@@ -18,18 +19,6 @@ class DeviceMap extends Component {
     super(props);
     this.state = { geoJson: null };
   }
-
-  // componentDidMount() {
-  //   this.addScript(this.props);
-  // }
-  //
-  // addScript(nextProps) {
-  //   if (!this.createdScript && nextProps.BingMapKey && nextProps.BingMapKey !== config.STATUS_CODES.STATIC) {
-  //     this.createdScript = true;
-  //     this.createScript( 'https://www.bing.com/api/maps/mapcontrol?callback=loadMap' );
-  //   }
-  // }
-  //
 
   componentWillReceiveProps(nextProps) {
     this.applyPropsToState(nextProps);
@@ -45,7 +34,8 @@ class DeviceMap extends Component {
     const geoJson = {type: "FeatureCollection", features: []};
       //If control reaches here, that means map is loaded and also the data is also loaded.
     geoJson.features = devices.items.map(device => {
-      let latitude, longitude, severity;
+      let id, latitude, longitude, severity;
+      id = device.Id;
       telemetryByDeviceGroup.Items.some(telemetryGroup => {
         /**
         Bing Map renders the devices only if the devices have longitude and latitude.
@@ -72,9 +62,11 @@ class DeviceMap extends Component {
         }
         return true; //stop looping
       });
+
       return {
         "type": "Feature",
          "properties": {
+           "description": id,
            severity: severity
          },
          "geometry": {
@@ -83,141 +75,9 @@ class DeviceMap extends Component {
          }
       };
     });
+
     this.setState({ geoJson : geoJson });
   }
-
-  // showMap(props) {
-  //   this.setState({ mapCallbackComplete: true });
-  //   const { devices, telemetryByDeviceGroup, alarmList, actions } = props;
-  //
-  //   if (!devices || !telemetryByDeviceGroup || !alarmList)  return;
-  //
-  //   //If control reaches here, that means map is loaded and also the data is also loaded.
-  //   devices.items.forEach(device => {
-  //     telemetryByDeviceGroup.Items.forEach(telemetryGroup => {
-  //       /**
-  //       Bing Map renders the devices only if the devices have longitude and latitude.
-  //       If not we are not showing the devices on Map (all devices don't have the longitude and latitude).
-  //       */
-  //       if (device.Id === telemetryGroup.DeviceId) {
-  //         if (
-  //           telemetryGroup.Data &&
-  //           telemetryGroup.Data.latitude &&
-  //           telemetryGroup.Data.longitude
-  //         ) {
-  //           device.latitude = telemetryGroup.Data.latitude;
-  //           device.longitude = telemetryGroup.Data.longitude;
-  //         } else if (device.Properties.Reported) {
-  //           device.latitude = device.Properties.Reported.Latitude;
-  //           device.longitude = device.Properties.Reported.Longitude;
-  //         }
-  //       }
-  //     });
-  //
-  //     alarmList.forEach(alarm => {
-  //       if (device.Id === alarm.DeviceId) {
-  //         device.Severity = alarm.Rule.Severity;
-  //       }
-  //     });
-  //   });
-  //   MapPane.setData({
-  //     BingMapKey: props.BingMapKey,
-  //     deviceData: devices.items,
-  //     container: this,
-  //     actions
-  //   });
-  //   let data = this.getDeviceData(devices.items);
-  //   MapPane.setDeviceLocationData(
-  //     data.minLat,
-  //     data.minLong,
-  //     data.maxLat,
-  //     data.maxLong,
-  //     data.locationList
-  //   );
-  // }
-  //
-  // getDeviceData(data) {
-  //   let Location = function(deviceId) {
-  //     this.deviceId = deviceId;
-  //     this.latitude = null;
-  //     this.longitude = null;
-  //     this.status = null;
-  //     this.Severity = null;
-  //   };
-  //   let minLat;
-  //   let maxLat;
-  //   let minLong;
-  //   let maxLong;
-  //   let offset = 0.05;
-  //   let mapData = {};
-  //   let longitudes = [];
-  //   let latitudes = [];
-  //   let locationList;
-  //   if (data && data.length) {
-  //     locationList = data.map(function(item, i) {
-  //       let location = new Location(item.Id);
-  //       if (item.Id.match(/10_/)) {
-  //         location.status = 1;
-  //       } else if (item.Id.match(/6_/)) {
-  //         location.status = 2;
-  //       } else {
-  //         location.status = 0;
-  //       }
-  //
-  //       location.Severity = item.Severity;
-  //       let latitude = (location.latitude = item.latitude);
-  //       let longitude = (location.longitude = item.longitude);
-  //       latitudes.push(Number(latitude));
-  //       longitudes.push(Number(longitude));
-  //
-  //       return location;
-  //     });
-  //   }
-  //   minLat = Math.min.apply(null, latitudes.filter(e => !isNaN(e)));
-  //   maxLat = Math.max.apply(null, latitudes.filter(e => !isNaN(e)));
-  //   minLong = Math.min.apply(null, longitudes.filter(e => !isNaN(e)));
-  //   maxLong = Math.max.apply(null, longitudes.filter(e => !isNaN(e)));
-  //
-  //   // TODO: check mark validate data after API integration
-  //   if (locationList && locationList.Count === 0) {
-  //     minLat = 47.6;
-  //     maxLat = 47.6;
-  //     minLong = -122.3;
-  //     maxLong = -122.3;
-  //   }
-  //   mapData.minLat = minLat - offset;
-  //   mapData.maxLat = maxLat + offset;
-  //   mapData.minLong = minLong - offset;
-  //   mapData.maxLong = maxLong + offset;
-  //   mapData.locationList = locationList;
-  //   return mapData;
-  // }
-  //
-  // createScript(src) {
-  //   return new Promise((resolve, reject) => {
-  //     let script = document.createElement('script');
-  //     script.src = src;
-  //     script.addEventListener('load', function() {
-  //       resolve();
-  //     });
-  //     script.addEventListener('error', function(e) {
-  //       reject(e);
-  //     });
-  //     document.body.appendChild(script);
-  //   });
-  // }
-  //
-  // renderStaticMap() {
-  //     return <div className="map-container">
-  //       <div className="static-map-modal">
-  //         <span className="static-text">
-  //           {lang.MAP_MODAL_CONTENT}
-  //           <a className="static-link" rel="noopener noreferrer" href="https://docs.microsoft.com/azure/iot-suite/iot-suite-faq" target='_blank'>{lang.MAP_MODAL_FAQS}</a>
-  //         </span>
-  //       </div>
-  //       <img src={StaticMapWithOutDevices} alt="StaticMap" className="static-bing-map"/>
-  //     </div>
-  // }
 
   render() {
     return (
--- src/components/deviceMap/lbsMap.js
+++ src/components/deviceMap/lbsMap.js
@@ -1,18 +1,42 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import * as clusterIcons from './clusterIcons';
+import EventTopic, { Topics } from '../../common/eventtopic';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions';
+
 import './deviceMap.css';
 
 class LbsMap extends Component {
+  constructor(props) {
+    super(props);
+    this.map = null;
+    this.state = {}
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.map) {
+      const source = this.map.getSource("earthquakes");
+      if (source) {
+        source.setData(nextProps.geoJson);
+      }
+    }
+  }
 
   componentDidMount() {
     mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-    const map=  new mapboxgl.Map({
+    const map = this.map =  new mapboxgl.Map({
       container: 'lbsMapElId',
-      style: 'mapbox://styles/mapbox/dark-v9'
+      style: 'mapbox://styles/mapbox/dark-v9',
+      center: [ -120.740135, 47.751076 ],
+      zoom: 5,
+      scrollZoom: true,
+      doubleClickZoom: true
     });
+
     map.addControl(new mapboxgl.NavigationControl());
+
     map.on('load', () => {
         map.addSource("earthquakes", {
             type: "geojson",
@@ -32,8 +56,8 @@ class LbsMap extends Component {
                     property: "point_count",
                     type: "interval",
                     stops: [
-                        [0, "gray"],
-                        [10, "#ffff99"],
+                        [0, "white"],
+                        [10, "white"],
                         [50, "#c2d6d6"]
                     ]
                 },
@@ -56,7 +80,6 @@ class LbsMap extends Component {
             filter: ["has", "point_count"],
             layout: {
                 "text-field": "{point_count_abbreviated}",
-                "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
                 "text-size": 12
             }
         });
@@ -67,13 +90,45 @@ class LbsMap extends Component {
             source: "earthquakes",
             filter: ["!has", "point_count"],
             paint: {
-                "circle-color": "#11b4da",
-                "circle-radius": 5,
-                "circle-stroke-width": 1,
-                "circle-stroke-color": "#fff"
+              "circle-color": "#11b4da",
+              "circle-radius": 7,
+              "circle-stroke-width": 1,
+              "circle-stroke-color": "#fff"
             }
         });
     });
+
+    map.on('click', 'unclustered-point', function (e) {
+       new mapboxgl.Popup()
+         .setLngLat(e.features[0].geometry.coordinates)
+         .setHTML(e.features[0].properties.description)
+         .addTo(map);
+    });
+
+     // Change the cursor to a pointer when the mouse is over the unclustered-point layer.
+     map.on('mouseenter', 'unclustered-point', function () {
+         map.getCanvas().style.cursor = 'pointer';
+     });
+
+     // Change it back to a pointer when it leaves.
+     map.on('mouseleave', 'unclustered-point', function () {
+         map.getCanvas().style.cursor = 'pointer';
+     });
+
+     let devices = this.props.devices;
+     let actions = this.props.actions;
+
+     // Flyout appeares when we click on default (final) pushpin.
+     map.on('click', 'unclustered-point', function (e) {
+       setTimeout(() => {
+         let device = devices.items.filter(item => {
+            return item.Id === e.features[0].properties.description;
+         });
+         const flyoutConfig = { device : device[0], type: 'Device detail' };
+         actions.showFlyout({ ...flyoutConfig });
+         EventTopic.publish(Topics.device.selected, device[0]);
+       });
+     });
   }
 
   render() {
@@ -81,4 +136,16 @@ class LbsMap extends Component {
   }
 }
 
-export default LbsMap;
+const mapStateToProps = state => {
+  return {
+    devices: state.deviceReducer.devices,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LbsMap);
